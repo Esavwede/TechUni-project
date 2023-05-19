e1e4f25ac8df0837d42029703807c0e8e7ebefb2
odomaurice <odomaurice@yahoo.com>
2023-05-19 07:16:46 +0100

    updates on the tailwind configuration and header

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -17,8 +17,9 @@ module.exports = {
         },
       },
       screens: {
-        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
-        tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
+       
+        // widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        // tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
       },
       keyframes: {
         "open-menu": {
