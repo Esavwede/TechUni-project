abe520d0594a0d6a6d8ebfa143b5f29d7f1ad6d3
odomaurice <odomaurice@yahoo.com>
2023-06-19 13:23:37 +0100

    Added testimonial section and updated the student

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,26 +1,32 @@
 const initApp = () => {
-  const hamburgerBtn = document.getElementById('hamburger-button')
-  const mobileMenu = document.getElementById('mobile-menu')
+  const hamburgerBtn = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
 
   const toggleMenu = () => {
-    mobileMenu.classList.toggle('hidden')
-    mobileMenu.classList.toggle('flex')
-    hamburgerBtn.classList.toggle('toggle-btn')
-  }
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+    hamburgerBtn.classList.toggle("toggle-btn");
+  };
 
-  hamburgerBtn.addEventListener('click', toggleMenu)
-  mobileMenu.addEventListener('click', toggleMenu)
+  hamburgerBtn.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", toggleMenu);
 
-  //  typed js effect starts
-//   var typed = new Typed('.typing-text', {
-//     strings: [
-//       'April 13th 2023'
-//     ],
-//     loop: false,
-//     typeSpeed: 50,
-//     backSpeed: 25,
-//     backDelay: 500
-//   })
+  // <!-- typed js effect starts -->
+  var typed = new Typed(".typing-text", {
+    strings: [
+      "Next Cohort Begins On July 3RD",
+      "Next Cohort Begins On July 3RD",
+      "Next Cohort Begins On July 3RD",
+      "Next Cohort Begins On July 3RD",
+      "Next Cohort Begins On July 3RD",
+    ],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+  });
+  // <!-- typed js effect ends -->
+  
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
