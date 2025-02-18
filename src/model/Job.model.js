50998451c900e245d2c2b0d80ecd177a0a7ee843
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-02-18 17:43:10 +0100

    fixes

--- src/model/Job.model.js
+++ src/model/Job.model.js
@@ -1,52 +1,52 @@
 
 
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema 
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema 
 
 
 
-
-const JobSchema = new Schema 
-    (
-        {
-            title:
-            {
-                type: String, 
-                required: true 
-            },
-            role: 
-            {
-                type: String, 
-                required: true 
-            },
-            location: {
-                 type: String,
-                 required: true,
-            },
-            salary:
-            {
-                type: String, 
-                required: true 
-            },
+// // 
+// const JobSchema = new Schema 
+//     (
+//         {
+//             title:
+//             {
+//                 type: String, 
+//                 required: true 
+//             },
+//             role: 
+//             {
+//                 type: String, 
+//                 required: true 
+//             },
+//             location: {
+//                  type: String,
+//                  required: true,
+//             },
+//             salary:
+//             {
+//                 type: String, 
+//                 required: true 
+//             },
            
-            content: 
-            {
-                type: String, 
-                required: true 
-            },
-            link:
-            {
-                type: String, 
-                required: true 
-            },
+//             content: 
+//             {
+//                 type: String, 
+//                 required: true 
+//             },
+//             link:
+//             {
+//                 type: String, 
+//                 required: true 
+//             },
             
-        },
-        {
-            timestamps: true 
-        }
-    )
+//         },
+//         {
+//             timestamps: true 
+//         }
+//     )
 
 
 
-const Job = mongoose.model('job', JobSchema,'jobs')
-module.exports = Job 
+// const Job = mongoose.model('job', JobSchema,'jobs')
+// module.exports = Job 
--- src/model/User.model.js
+++ src/model/User.model.js
@@ -1,58 +1,58 @@
 
 
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema 
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema 
 
 
-const UserSchema = new Schema 
-    (
-        {
-           firstname: 
-           {
-            type: String, 
+// const UserSchema = new Schema 
+//     (
+//         {
+//            firstname: 
+//            {
+//             type: String, 
             
-           },
-           lastname: 
-           {
-            type: String, 
+//            },
+//            lastname: 
+//            {
+//             type: String, 
       
-           },
-           email: 
-           {
-            type: String, 
-            required: true 
-           }, 
-           password: 
-           {
-            type: String, 
-            required: true 
-           },
-           emailVerificationLink: 
-           {
-            type: String, 
+//            },
+//            email: 
+//            {
+//             type: String, 
+//             required: true 
+//            }, 
+//            password: 
+//            {
+//             type: String, 
+//             required: true 
+//            },
+//            emailVerificationLink: 
+//            {
+//             type: String, 
            
-           }, 
-           emailVerified: 
-           {
-            type: String, 
+//            }, 
+//            emailVerified: 
+//            {
+//             type: String, 
             
-           },
-           isAdmin: 
-           {
-            type: String, 
+//            },
+//            isAdmin: 
+//            {
+//             type: String, 
             
-           }
-        },
-        {
-            timestamps: true 
-        }
-    )
+//            }
+//         },
+//         {
+//             timestamps: true 
+//         }
+//     )
 
 
-const User = mongoose.model('user', UserSchema,'users')
+// const User = mongoose.model('user', UserSchema,'users')
 
-const userDoc = { email:"glover@gmail.com", password:"password" }
-const newUser = new User(userDoc) 
-newUser.save()
+// const userDoc = { email:"glover@gmail.com", password:"password" }
+// const newUser = new User(userDoc) 
+// newUser.save()
 
-module.exports = User  
+// module.exports = User  
