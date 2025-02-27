87097c66157f10bfdf959a409fe796343dd510f5
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-02-27 12:24:06 +0100

    fix: removed production logger

--- src/system/logger/prodLogger.js
+++ src/system/logger/prodLogger.js
@@ -1,16 +1,25 @@
 
 
 const pino = require('pino') 
+
 const prodLogger = pino(
-    {
-        transport: 
         {
-            target: 'pino/file',
-            options: { destination: `${ __dirname }/logs/prod/logs.txt`}
-        },
-        level: process.env.APPLICATION_LOG_LEVEL || 'info', 
-        timestamp: pino.stdTimeFunctions.isoTime 
-    }
-) 
+            transport:
+            {
+               targets: 
+               [
+                 { 
+                    target: 'pino-pretty'
+                 }
+               //   {
+               //      target: 'pino/file', 
+               //      options: { destination: `${ __dirname }/logs/dev/logs.txt`}
+               //   }
+               ]
+            },
+            level: process.env.APPLICATION_LOG_LEVEL || 'info', 
+            timestamp: pino.stdTimeFunctions.isoTime 
+        }
+    ) 
 
 module.exports =  prodLogger 
